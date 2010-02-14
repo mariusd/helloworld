5d528024c82c4ca01e1920cd4ce65d476e77e0b7
Marius Damarackas <m.damarackas@gmail.com>
2010-02-14 02:15:43 +0200

    Added quadratic.py -- module for solving quadratic equation.

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+
+import quadratic
 
 def main():
     print "Sveikas, Pasauli!"
+    print "phi =", quadratic.solve_quadratic(1, -1, -1)[1]
 
 if __name__ == "__main__":
     main()
---
+++ quadratic.py
@@ -0,0 +1,32 @@
+"""Module contains a function that solves quadratic equation."""
+
+import math
+import cmath
+
+def solve_quadratic(a, b, c):
+    """Find real or complex roots of equation ax**2 + bx + c = 0.
+
+    >>> solve_quadratic(0, 2, 1)
+    Traceback (most recent call last):
+    ...
+    ValueError: equation is not quadratic
+
+    >>> solve_quadratic(1, -4, 4)
+    (2.0,)
+
+    >>> solve_quadratic(1, -5, 6)
+    (2.0, 3.0)
+
+    >>> [solve_quadratic(1, 0, k**2) for k in range(1, 5)]
+    [(-1j, 1j), (-2j, 2j), (3j, -3j), (4j, -4j)]
+    """
+    if a == 0:
+        raise ValueError("equation is not quadratic")
+    discriminant = b**2 - 4 * a * c
+    sqrt = lambda x : math.sqrt(x) if x >= 0 else cmath.sqrt(x)
+    roots = ((-b + k * sqrt(discriminant)) / (2.0 * a) for k in (-1, 1))
+    return tuple(set(roots))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
